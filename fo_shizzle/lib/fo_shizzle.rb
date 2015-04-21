86e5c975329fada6aa6891a10f8d2a0145352e46
Steve (Tim) Bussert & Frank (Gene) Johnson <student@epicodus.com>
2015-04-20 17:04:08 -0700

    Fo_Shizzle works with 1 word saved. Doesn't work with sentence below binding.

--- fo_shizzle/lib/fo_shizzle.rb
+++ fo_shizzle/lib/fo_shizzle.rb
@@ -1,24 +1,30 @@
+# require('pry')
+#
 # class String
 #   define_method(:fo_shizzle) do
 #     sentence = self.split
+#     word = ""
 #     range1 = (0..sentence.length())
 #     range1.each() do |i1|
 #       word = sentence[i1]
 #       word2 = word.split("")
-#
-#       range2 = (1..word2.length())
+#       range2 = (1..(word.length() -1))
 #       range2.each() do |i2|
 #         if word2[i2] == 's'
 #           word2[i2] = 'z'
 #         end
+#         word = word2.join("")
 #       end
-#       sentence[i1] = word2.join("")
+#
+#       sentence[i1] = word
+# binding.pry
 #     end
+#
 #     sentence = sentence.join(" ")
 #   end
 # end
 #
-#
+
 class String
   define_method(:fo_shizzle) do
     word = self.split("")
@@ -31,3 +37,16 @@ class String
     word = word.join("")
   end
 end
+
+# class String
+#   define_method(:fo_shizzle) do
+#     word = self.split("")
+#     range = (0..word.length())
+#     range.each() do |i|
+#       if word[i] == 's'
+#         word[i] = 'z'
+#       end
+#     end
+#     word = word.join("")
+#   end
+# end
